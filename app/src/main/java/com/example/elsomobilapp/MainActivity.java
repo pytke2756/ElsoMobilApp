05439fca1c316235dafa7e552e47f6956df1600b
Ibrányi Levente <ibranyilevente@gmail.com>
2021-11-15 16:48:16 +0100

    MainActivity.java create

--- app/src/main/java/com/example/elsomobilapp/MainActivity.java
+++ app/src/main/java/com/example/elsomobilapp/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.elsomobilapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnBevitel;
+    private EditText etNev;
+    private TextView txtHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        btnBevitel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nev = etNev.getText().toString();
+                String koszones = "Hello " + nev;
+                txtHello.setText(koszones);
+            }
+        });
+    }
+
+    private void init(){
+        btnBevitel = findViewById(R.id.btn_bevitel);
+        etNev = findViewById(R.id.et_nev);
+        txtHello = findViewById(R.id.txt_hello);
     }
 }
